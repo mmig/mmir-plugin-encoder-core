acba11fdce4a9a782b487693bc7a9e36209873c3
russa <aaron.russ@dfki.de>
2019-02-12 11:56:25 +0100

    converted to AMD module

--- www/recorderExt.js
+++ www/recorderExt.js
@@ -2,37 +2,37 @@
  * License (MIT)
  *
  * modifications:
- * 
+ *
  * 	Copyright (C) 2013 DFKI GmbH
  * 	Deutsches Forschungszentrum fuer Kuenstliche Intelligenz
  * 	German Research Center for Artificial Intelligence
  * 	http://www.dfki.de
- *  
+ *
  * based on
  * 	Copyright (C) 2013 Matt Diamond (MIT License)
  *
- * 	Permission is hereby granted, free of charge, to any person obtaining a 
- * 	copy of this software and associated documentation files (the 
- * 	"Software"), to deal in the Software without restriction, including 
- * 	without limitation the rights to use, copy, modify, merge, publish, 
- * 	distribute, sublicense, and/or sell copies of the Software, and to 
- * 	permit persons to whom the Software is furnished to do so, subject to 
+ * 	Permission is hereby granted, free of charge, to any person obtaining a
+ * 	copy of this software and associated documentation files (the
+ * 	"Software"), to deal in the Software without restriction, including
+ * 	without limitation the rights to use, copy, modify, merge, publish,
+ * 	distribute, sublicense, and/or sell copies of the Software, and to
+ * 	permit persons to whom the Software is furnished to do so, subject to
  * 	the following conditions:
- * 
- * 	The above copyright notice and this permission notice shall be included 
+ *
+ * 	The above copyright notice and this permission notice shall be included
  * 	in all copies or substantial portions of the Software.
- * 
- * 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- * 	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- * 	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * 	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
- * 	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- * 	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ *
+ * 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * 	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * 	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * 	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * 	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * 	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * 	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 
-(function(window){
+define(function(){
 
   var WORKER_PATH = 'mmirf/workers/recorderWorker.js';
 
@@ -49,10 +49,10 @@
 //    	}
 //    });
     this.processor = worker;///////////TODO MOD
-    
+
     var recording = false,
       currCallback;
-    
+
     var dataListener;//TODO MOD
 
     var doRecordAudio = function(e){
@@ -65,31 +65,31 @@
         ]
       });
     }
-    
+
     this._initSource = function(inputSource){
 
         this.context = inputSource.context;
-        
+
         //if we already had another input-source before, we need to clean up first:
         if(this.node){
         	this.node.disconnect();
         }
-        
-        //backwards compatibility: use older createJavaScriptNode(), if new API function is not available 
+
+        //backwards compatibility: use older createJavaScriptNode(), if new API function is not available
         var funcName = !this.context.createScriptProcessor? 'JavaScriptNode' : 'ScriptProcessor';
         this.node = this.context['create'+funcName](bufferLen, 2, 2);
 
     	this.node.onaudioprocess = doRecordAudio;
-    	
+
     	worker.postMessage({
         	cmd: 'init',
         	config: {
         		sampleRate: this.context.sampleRate
         	}
         });
-    	
+
     	inputSource.connect(this.node);
-        this.node.connect(this.context.destination); 
+        this.node.connect(this.context.destination);
     }
 
     this.configure = function(cfg){
@@ -151,33 +151,33 @@
     this.doEncode = function(){
     	worker.postMessage({ cmd: 'encode' });
     };
-    
+
     this.doFinish = function(){
     	worker.postMessage({ cmd: 'encClose'});
     };
-    
+
     this.init = function(source){
     	this.destroyed = false;
         this._initSource(source);
     };
-    
+
     this.release = function(){
-    	
+
     	this.stop();
     	this.clear();
-    	
+
     	if(this.node){
         	this.node.disconnect();
         	this.node = null;
         }
-    	
+
     	this.destroyed = true;
     };
 	///////////TODO MOD end
-    
+
     var selfRef = this;///////////TODO MOD
     worker.onmessage = function(e){
-    	
+
     	///////////TODO MOD start
     	if(e.data && e.data.cmd === 'encFinished'){
     	  	if(selfRef.onencodefinished){
@@ -198,7 +198,7 @@
     		return;
     	}
     	///////////TODO MOD end
-    	
+
     	///////////TODO MOD start
     	var result = true;
     	if(selfRef.beforeonmessage){
@@ -208,14 +208,14 @@
     		return;
     	}
     	///////////TODO MOD end
-    	
+
     	currCallback(blob);
-    	
+
     }
 
 //    source.connect(this.node);
 //    this.node.connect(this.context.destination);    // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
-    
+
     this._initSource(source);
   };
 
@@ -231,5 +231,7 @@
     link.dispatchEvent(click);
   }
 
-  window.Recorder = Recorder;
-})(window);
+  // window.Recorder = Recorder;
+
+  return Recorder;
+});
