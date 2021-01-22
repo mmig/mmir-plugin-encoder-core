10b980688c54a7b3e1105801fa4225c71d2b1225
russa <aaron.russ@dfki.de>
2021-01-22 21:58:12 +0100

    add dynamic build-config for wav encoder

---
+++ build-config.ts
@@ -0,0 +1,15 @@
+
+/**
+ * build configuration for the plugin:
+ * exported AppConfig variables will be included in generated module-config.gen.js
+ */
+
+import { PluginExportBuildConfigCreator } from 'mmir-tooling';
+
+export const buildConfigLibDependencies: PluginExportBuildConfigCreator = function(pluginConfig, _runtimeConfig, _pluginBuildConfigs) {
+  if(pluginConfig && pluginConfig.encoder === 'wav'){
+    return {
+      includeModules: ['mmir-plugin-encoder-core/workers/recorderWorkerExt']
+    }
+  }
+};
