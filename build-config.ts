9ed511b1c3eed316116979557508252eaedf0a36
russa <aaron.russ@dfki.de>
2021-02-27 00:33:58 +0100

    added build config for known encoder plugins

--- build-config.ts
+++ build-config.ts
@@ -7,9 +7,17 @@
 import { PluginExportBuildConfigCreator } from 'mmir-tooling';
 
 export const buildConfigLibDependencies: PluginExportBuildConfigCreator = function(pluginConfig, _runtimeConfig, _pluginBuildConfigs) {
-  if(pluginConfig && pluginConfig.encoder === 'wav'){
-    return {
-      includeModules: ['mmir-plugin-encoder-core/workers/wavEncoder']
+  if(pluginConfig && pluginConfig.encoder){
+    var enc = pluginConfig.encoder;
+    if(enc === 'wav'){
+      return {
+        includeModules: ['mmir-plugin-encoder-core/workers/wavEncoder']
+      }
+    }
+    if(enc === 'amr' || enc === 'flac' || enc === 'opus' || enc === 'speex'){
+      return {
+        includePlugins: [{id: 'mmir-plugin-encoder-'+enc}]
+      }
     }
   }
 };
